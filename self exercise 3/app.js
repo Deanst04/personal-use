f664a3eb6f6a3b787746d1467d312dc7cd084f5a
Dean Stark <deanstark04@gmail.com>
2025-08-05 09:55:13 +0300

    try

---
+++ self exercise 3/app.js
@@ -0,0 +1,12 @@
+"use strict";
+
+(() => {
+  const users = [
+    { name: "david", age: 21 },
+    { name: "sarah", age: 17 },
+    { name: "moshe", age: 34 },
+    { name: "ronit", age: 16 },
+    { name: "yoav", age: 45 },
+  ];
+
+})();
