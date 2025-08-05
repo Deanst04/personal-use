9af84723136694f361d5a50aa2d2341c8be62bd5
Dean Stark <deanstark04@gmail.com>
2025-08-05 10:41:12 +0300

    trying again...

--- self exercise 3/app.js
+++ self exercise 3/app.js
@@ -9,4 +9,22 @@
     { name: "yoav", age: 45 },
   ];
 
+  const getAverageAdultsAge = users => {
+    const { totalAge, adults} = users
+      .reduce((acc, {age}) => {
+        const current = {...acc}
+        if (age > 18) {
+          current.totalAge += age
+          current.adults += 1
+        } else {
+          current.totalAge = age
+          current.adults = 1
+        }
+        return current
+      }, { totalAge, adults})
+      const average = totalAge / adults
+      return `the average adults age is: ${average}`
+  }
+  
+  console.log(getAverageAdultsAge(users))
 })();
